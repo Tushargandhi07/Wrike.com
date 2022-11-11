85d577e087dd2a09cbc8bc646b68093973569b60
Tushargandhi07 <tushargan7@gmail.com>
2022-11-11 19:46:26 +0530

    day-4 work done

---
+++ price.js
@@ -0,0 +1,54 @@
+let container = document.querySelector("#container")
+    let pricing_data = [];
+    let url = "https://636dbafab567eed48ac79c97.mockapi.io/plan";
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            displayCard(data);
+            pricing_data = data;
+        });
+
+    function displayCard(data) {
+        container.innerHTML = ""
+        data.forEach((ele, index) => {
+            let div = document.createElement("div");
+            let department = document.createElement("h1");
+            department.innerText = ele.department;
+
+            let comment = document.createElement("h3");
+            comment.innerText = ele.comment;
+
+            let des = document.createElement("p");
+            des.innerText = ele.discription;
+
+            let price = document.createElement("h2");
+            price.innerText = ele.price;
+
+            let validity = document.createElement("p");
+            validity.innerText = "user/month";
+
+
+            let Try = document.createElement("button");
+            Try.innerText = "Try for free";
+            Try.addEventListener("click", function () {
+                addtocart(ele);
+            })
+
+            div.append(department, comment, des, price, validity, Try);
+            container.append(div);
+
+        })
+    }
+
+    function addtocart(data) {
+        let cart = JSON.parse(localStorage.getItem("cartData")) || [];
+        for (let i = 0; i < cart.length; i++) {
+            if (cart[i].department == data.department) {
+                alert("Already in cart");
+                return;
+            }
+        }
+        cart.push(data);
+        localStorage.setItem("cartData", JSON.stringify(cart));
+        alert("Plan added to cart");
+    }
